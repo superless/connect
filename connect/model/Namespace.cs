d229a4bb5c1b3fbc248be7fc89b2ef71f02cc859
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-07 21:41:01 -0300

    Se elimina delete item, está en mdm

--- connect/model/Namespace.cs
+++
@@ -1,9 +0,0 @@
-namespace trifenix.connect.Model
-{
-    /// <summary>
-    /// dentro de model estarán las clases abstractas del modelo de trifenix
-    /// Desde DocumentDb se heredarán todos los elementos del modelo.
-    /// </summary>
-    internal class Namespace
-    {}
-}
