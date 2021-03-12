c1a81cadcf53045945d0f724cd27ce788de40edf
yo-fenix <alejandro.iglesias@trifenix.io>
2021-03-12 20:53:29 -0300

    EsperandoToValue

--- connect/util/Mdm.cs
+++ connect/util/Mdm.cs
@@ -127,17 +127,137 @@ namespace trifenix.connect.util
             // si existe solo uno formarlo desde el tipo y el valor.
 
         }
-            
+
+        public static (ToValue toValue, (Type type, ToGlobalFilterValueAttribute toValueAttr)[] tup) ToValueFirstStep((Type[] types, (Type type, ToGlobalFilterValueAttribute toValueAttr)[] tup) { 
+        
+                
+        }
+
+        public static ToValue GetToValue(Type type, ToGlobalFilterValueAttribute toValueAttr) { 
+            return new ToValue { 
+                Index = Reflection.Entities.GetIndex(toValueAttr.SourceType),
+
+            }            
+        }
+
 
         /// <summary>
-        /// 
+        /// Usa ToProcessClass o ToValue
+        /// para generar el contenedor (ToProcess o ToValue) con el primer registro
+        /// (donde el tipo sea igual a la fuente del ToGlobalFilterValueAttribute)        
         /// </summary>
-        /// <param name="tup"></param>
-        /// <returns></returns>
-        public static (T toValue, (Type type, ToGlobalFilterValueAttribute toValueAttr)[] tup) ToValueFirstStep<T>((Type type, ToGlobalFilterValueAttribute toValueAttr)[] tup, Func<Type, ToGlobalFilterValueAttribute, T> convert) where T: PathCollection
-        { 
-        
+        /// <param name="types">tipos en el modelo de datos</param>
+        /// <param name="tup">colección de tuplas con el tipo y el atributo para llegar a un valor</param>
+        /// <param name="convert">Convierte una tupla de type y ToGlobalFilterValue del primer salto (origen) a el tipo contenedor (ToProcess o ToValue)</param>
+        /// <returns>el contenedor creando la primera estructura y el resto de tuplas con el tipo y el atributo ToGlobalFilterValue</returns>
+        public static (T toValue, (Type type, ToGlobalFilterValueAttribute toValueAttr)[] tup) ToPathFirstStep<T>(Type[] types, (Type type, ToGlobalFilterValueAttribute toValueAttr)[] tup, Func<Type, ToGlobalFilterValueAttribute, T> convert) where T: PathCollection
+        {
+            var firstJump = tup.First(s => s.toValueAttr.SourceType.Equals(s.type));
+
+            var (typeFistJump, pathFirstJump) = firstJump;
+
+            var container = convert(typeFistJump, pathFirstJump);
+
+            var paths = container.PathToProcess.ToList();
+
+            var nextTarget = GetNextTarget(types, firstJump.toValueAttr.SourceType, firstJump.toValueAttr.TargetType);
+
+            paths.Add(new PathToFiltersValue {
+                OriginClass = firstJump.toValueAttr.SourceType.Name,
+                OriginIndex = Reflection.Entities.GetIndex(firstJump.type).Value,
+                PropertyName = nextTarget.propInfo.Name, // en typescript empieza con minuscula.
+                TargetClass = nextTarget.type.Name,
+                TargetIndex = Reflection.Entities.GetIndex(nextTarget.type).Value
+            });
+
+
+
+            var tupList = tup.ToList();
+
+            tupList.Remove(firstJump);
+
+            return (container, tupList.ToArray());
         }
+
+
+        /// <summary>
+        /// Obtiene el nombre de la propiedad en la entidad destino que apunta a la entidad fuente.
+        /// </summary>
+        /// <param name="types">donde buscar los tipos aledaños</param>
+        /// <param name="source">tipo que necesita el nombre de la propiedad</param>
+        /// <param name="target">Termino de la ruta</param>
+        /// <returns>Nombre de la propiedad</returns>
+        public static (Type type, PropertyInfo propInfo, EntityIndexRelatedPropertyAttribute entityAttr) GetNextTarget(Type[] types, Type source, Type target) {
+
+            // colección con clases secanas
+            var closes = GetCloseEntities(types, source);
+
+            return closes.First(s => s.type.Equals(target));
+
+
+        }
+
+
+        /// <summary>
+        /// Obtiene un listado de entidades (tipo) y los datos que se aplicaron en la metadata de la propiedade que se apunta.
+        /// </summary>
+        /// <param name="types">Donde buscará las entidades</param>
+        /// <param name="source">Tipo del que se buscará otras propiedades de otras entidades que aputan a el</param>
+        /// <returns>entidades que apuntan a la fuente</returns>
+        public static (Type type, PropertyInfo propInfo, EntityIndexRelatedPropertyAttribute entityAttr)[] GetCloseEntities(Type[] types, Type source) {
+
+            var entitiesWithRelated = GetAllEntitiesWithRelated(types);
+
+            return GetCloses(entitiesWithRelated, source);
+        }
+
+
+
+        /// <summary>
+        /// obtiene una colección con el tipo del elemento o entidad más cercanos y el atributo que apunta al origen
+        /// </summary>
+        /// <param name="tup">colección de tuplas con tipos que tienen propiedades que implmeneten el atributo related</param>
+        /// <param name="typeSource">tipo fuente, del que obtendremos los hermanos</param>
+        /// <returns>el listado de tipos aledaños y el atributo que apunta al origen</returns>
+        public static (Type type, PropertyInfo propInfo, EntityIndexRelatedPropertyAttribute entityAttr)[] GetCloses((Type type, PropertyInfo[] entityAttr)[] tup, Type typeSource) {
+
+            var index = Reflection.Entities.GetIndex(typeSource);
+
+            var tupWithEntity = tup.Select(s =>
+            {
+                return (s.type,s.entityAttr.Where(a => Reflection.Attributes.GetAttribute<EntityIndexRelatedPropertyAttribute>(a).Index == index), s.entityAttr.Select(a => Reflection.Attributes.GetAttribute<EntityIndexRelatedPropertyAttribute>(a)));
+            });
+
+            return tupWithEntity.Where(s => s.Item3.Any(a => a.Index == index)).Select(s =>
+            {
+                return (s.type, s.Item2.First(), s.Item3.First(f => f.Index == index));
+            }).ToArray();
+        }
+
+
+        /// <summary>
+        /// Obtiene todas las clases que tengan propiedades con el atributo related
+        /// </summary>
+        /// <param name="types">Todos los tipos del namespace del modelo</param>
+        /// <returns>Tipo de la clase que tiene las propiedades y  el propInfo de cada una que tenga el atributo</returns>
+        public static (Type type, PropertyInfo[] entityAttr)[] GetAllEntitiesWithRelated(Type[] types) {
+
+            return types.Select(s => {
+                return (s, Reflection.Attributes.GetAttributeList<EntityIndexRelatedPropertyAttribute>(s).Select(a=>a.Property).ToArray());
+            }).Where(s => {
+                var (t, v) = s;
+                return v != null;
+            }).ToArray();
+        }
+
+
+
+
+
+
+
+
+
 
 
 
